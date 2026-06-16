864052cfd18d67988e5930ec2fa6460b7a8410f7
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2026-06-16 12:56:55 +0000

    Completed adding the ability to asign person positions and delete the assignment. Added switch to show active and inactive people in the people table.

--- pages/People/jsobjects/utils/utils.js
+++ pages/People/jsobjects/utils/utils.js
@@ -9,16 +9,51 @@ export default {
 	},
 	async onTable1RowChange () {
 		this.loadingModalTrigger(true, "Fetching user details...")
+		resetWidget("Select_Position")
 		Table2.setData([])
 		Table3.setData([])
 
 		const [positions, qualifications] = await Promise.all([
 			Person_Positions.run(),
-			Person_Qualifications.run()
+			Person_Qualifications.run(),
+			All_Positions.run()
 		]).catch(this.loadingModalTrigger(false));
 
 		Table2.setData(positions);
 		Table3.setData(qualifications);
 		this.loadingModalTrigger(false);
+	},
+	async onPageLoad () {
+		this.loadingModalTrigger(true, "Fetching users...");
+		await Select_MV11_Workshop_People1.run();
+		await this.onTable1RowChange();
+	},
+	async assignPosition() {
+		this.loadingModalTrigger(true, "Assigning position...");
+		try {
+			await Add_Position.run({
+				position_id: Select_Position.selectedOptionValue,
+				person_id: Table1.selectedRow.id
+			})
+
+			await this.onTable1RowChange();
+		} catch (err) {
+			showAlert("Failed to assign position with the following error: " + err.message, "error")
+			this.loadingModalTrigger(false)
+		}
+	},
+	async deletePosition(){
+		this.loadingModalTrigger(true, "Deleting position...");
+		try {
+			Delete_Position.run({
+				position_id: Table2.triggeredRow.id,
+				person_id: Table1.selectedRow.id
+			})
+
+			await this.onTable1RowChange();
+		} catch (err) {
+			showAlert("Failed to delete position with the following error: " + err.message, "error")
+			this.loadingModalTrigger(false)
+		}
 	}
 }
