e84a0e32e23810c859eff1078a988e1fcb30a1e8
Daniel Lake <daniel.lake@millersvanguard.co.uk>
2026-06-19 09:48:16 +0000

    Completed a function to retries and compile slot times based on equipment from the database included relevant queries

---
+++ pages/Production Schedule/jsobjects/data/data.js
@@ -0,0 +1,17 @@
+export default {
+	async chartData (year = 2026) {
+		const slotsRaw = await Booking_Slots.run({year})
+		const slots = slotsRaw.map((slot) => ({
+			"Call_Id": slot.Call_Id,
+			"eq_description": slot.eq_description,
+			"bookings": JSON.parse(slot.bookings)
+		}))
+		
+		Booking_Slots.clear()
+
+		return {
+			"year": year,
+			"equipment": slots
+		}
+	}
+}
